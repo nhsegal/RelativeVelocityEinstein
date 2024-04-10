9b010d1783296353c77ac0aad60fa7b1779cef8c
Noah Segal <n.h.segal@gmail.com>
2024-04-10 10:32:11 -0400

    Car wheels spin without slipping

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function draw() {
 function createCar() {
   let positionX = width/2;
   let positionY = height/2;
-  let velocityX = 0;
+  let velocityX = 0.5;
   let sizeX = 195;
   let sizeY = 100; 
   let angle = 0;
@@ -35,7 +35,7 @@ function createCar() {
   const display = () => {
     image(car_img, positionX, positionY, sizeX, sizeY);
     ellipse(positionX+ sizeX*.32, positionY + sizeY*.15, 10, 10)
-   // image(wheel_img, positionX, positionY, sizeX, sizeY);
+
     push();
     translate(positionX - sizeX*.314, positionY + sizeY*.172)
     rotate(angle);
@@ -55,8 +55,8 @@ function createCar() {
   }
 
   const move = () => {
-    positionX = positionX + 0;
-    angle = angle + .1
+    positionX = positionX + velocityX;
+    angle = angle + .0003*sizeX*velocityX;
   }
 
   return { display, move, angle}
