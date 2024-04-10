5d80a3d7f960dd0223b8190b48d7def0260a9f79
Noah Segal <n.h.segal@gmail.com>
2024-04-10 13:41:15 -0400

    Add belt velocity

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 let car_img;
 let wheel_img;
 let car;
-let resetButton;
 let carVelocitySlider;
 
+let belt;
+let beltVelocitySlider;
+
+let resetButton;
 
 // Load the image.
 function preload() {
@@ -13,26 +16,41 @@ function preload() {
 
 
 function setup() {
-  createCanvas(800, 400);
-  background(220);
+  createCanvas(1400, 400);
+  background(255);
+  rectMode(CENTER)
   imageMode(CENTER);
+  textAlign(CENTER);
   car = createCar();
+  belt = createConveyorbelt();
   resetButton = createButton('Reset');
-  resetButton.position(0, 100);
+  resetButton.position(width/2 - 50, 4*height/5);
   resetButton.mousePressed(car.reset);
 
    // Create a slider and place it at the top of the canvas.
    carVelocitySlider = createSlider(-20, 20);
-   carVelocitySlider.position(10, 10);
-   carVelocitySlider.size(80);
+   carVelocitySlider.position(width/10, 4*height/5);
+   carVelocitySlider.size(160);
+   carVelocitySlider.value(0);
    describe('A dark gray square with a range slider at the top.');
- 
+
+   beltVelocitySlider = createSlider(-20, 20);
+   beltVelocitySlider.position(width/10, 4.5*height/5);
+   beltVelocitySlider.size(160);
+   beltVelocitySlider.value(0);
+   describe('A dark gray square with a range slider at the top.');
+   
+   textFont('Arial')
 }
 
 function draw() {
-  background(220);
+  background(255);
   car.setVelocity( carVelocitySlider.value()/10);
-  car.move()
+  belt.setVelocity( beltVelocitySlider.value()/10);
+  car.move();
+  belt.move();
+ 
+  belt.display();
   car.display()
 
 }
@@ -40,7 +58,7 @@ function draw() {
 function createCar() {
   let positionX = width/2;
   let positionY = height/2;
-  let velocityX = -1.0;
+  let velocityX = 0;
   let sizeX = 195;
   let sizeY = 100; 
   let angle = 0;
@@ -91,4 +109,39 @@ function createCar() {
     
   }
 
+function createConveyorbelt() {
+  let positionX = width/2;
+  let velocityX = -1.0;
+  let lineNumber = 100;
+  let spacing = 80;
 
+  const display = () => {
+    textSize(18);
+    fill(200)
+    noStroke()
+    push()
+    translate(positionX, height/2)
+    rect(0, height/12, width*2, height/6)
+  
+    for (let i = -lineNumber/2; i < lineNumber/2; i++){
+        noStroke()
+        fill(0);
+        text(`${i*10} cm`, spacing*2*i-25, height/6 + 18);
+        stroke(1)
+        strokeWeight(1)
+        line(spacing*i-40, 0, spacing*i, height/6)
+    }
+    pop()
+  }
+
+  const move = () => {
+    positionX = positionX + velocityX;
+  }
+
+  const setVelocity = (val) => {
+    velocityX = val;
+  }
+  return {display, move, setVelocity}
+
+
+}
