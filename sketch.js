1f1ccb593fc1124188753faa45f39ba47c2e4e38
Noah Segal <n.h.segal@gmail.com>
2024-04-10 10:29:25 -0400

    Car wheels spin

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let car_img;
+let wheel_img;
 let car;
 
 // Load the image.
 function preload() {
   car_img = loadImage('imgs/car.png');
+  wheel_img = loadImage('imgs/wheel.png');
 }
 
 
@@ -16,6 +18,8 @@ function setup() {
 }
 
 function draw() {
+  background(220);
+  car.move()
   car.display()
 
 }
@@ -23,18 +27,39 @@ function draw() {
 function createCar() {
   let positionX = width/2;
   let positionY = height/2;
-  let sizeX = 185;
+  let velocityX = 0;
+  let sizeX = 195;
   let sizeY = 100; 
+  let angle = 0;
+
   const display = () => {
-    
     image(car_img, positionX, positionY, sizeX, sizeY);
+    ellipse(positionX+ sizeX*.32, positionY + sizeY*.15, 10, 10)
+   // image(wheel_img, positionX, positionY, sizeX, sizeY);
+    push();
+    translate(positionX - sizeX*.314, positionY + sizeY*.172)
+    rotate(angle);
+    translate(-positionX + sizeX*.314, -positionY - sizeY*.172)
+    image(wheel_img, positionX, positionY, sizeX, sizeY);
+    pop();
+    
+    push();
+    translate(positionX + sizeX*.314, positionY + sizeY*.172)
+    rotate(angle);
+    translate(-positionX + sizeX*.314, -positionY - sizeY*.172)
+    image(wheel_img, positionX, positionY, sizeX, sizeY);
+    pop();
+
+
     describe('A red car');
   }
+
   const move = () => {
-    positionX = positionX + 1;
+    positionX = positionX + 0;
+    angle = angle + .1
   }
 
-  return { display }
+  return { display, move, angle}
     
   }
 
