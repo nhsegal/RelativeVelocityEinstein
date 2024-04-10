7e60863ab6e5ff4abbeaf5d7d40933c4bf3273c7
Noah Segal <n.h.segal@gmail.com>
2024-04-10 09:43:45 -0400

    Car image

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let car_img;
+
+// Load the image.
+function preload() {
+  car_img = loadImage('imgs/car.png');
+}
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
+  background(220);
+  imageMode(CENTER)
+  image(car_img, 200, 200, 185, 100);
+  describe('A red car');
+  
 }
 
 function draw() {
-  background(220);
+
 }
