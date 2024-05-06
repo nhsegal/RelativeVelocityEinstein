bb4b47e8fc906c60d189f4fd3e0f11776f3075b7
Noah Segal <n.h.segal@gmail.com>
2024-05-05 21:33:40 -0400

    position checkboxes

--- scripts/MakeCheckbox.js
+++ scripts/MakeCheckbox.js
@@ -1,8 +1,11 @@
 const makeCheckboxes = () => {
-  showclockscheckbox = createCheckbox('Show Conveyor Belt Clocks')
-  showclockscheckbox.position(40,20)
-  showdistancecheckbox = createCheckbox('Show Conveyor Belt Distance Markers')
-  showdistancecheckbox.position(40,40)
-  showvectorscheckbox = createCheckbox('Show Velocity Vectors')
-  showvectorscheckbox.position(40,360)
+  showclockscheckbox = createCheckbox('Conveyor Belt Clocks')
+  showclockscheckbox.parent('canvasDiv')
+  showclockscheckbox.position(40,-660, 'relative')
+  showdistancecheckbox = createCheckbox('Conveyor Belt Distance Markers')
+  showdistancecheckbox.parent('canvasDiv')
+  showdistancecheckbox.position(40,-655, 'relative')
+  showvectorscheckbox = createCheckbox('Velocity Vectors')
+  showvectorscheckbox.parent('canvasDiv')
+  showvectorscheckbox.position(1240,-690, 'relative')
 }
